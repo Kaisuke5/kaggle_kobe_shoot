6da21a13a30b62ef325dc7a5b8c049e73204119d
Kaisuke5 <kaisuke5.roy7@gmail.com>
2016-04-25 17:25:04 +0900

    fda

---
+++ src/model.py
@@ -0,0 +1,77 @@
+import numpy as np
+import six
+import chainer.links as L
+import chainer
+print chainer.__version__
+from chainer import computational_graph as c
+from chainer import cuda
+import chainer.functions as F
+from chainer import optimizers
+
+
+class network(chainer.Chain):
+
+	def __init__(self, n_in, n_units, n_out):
+		super(network, self).__init__(
+			l1=L.Linear(n_in, n_units),
+			l2=L.Linear(n_units, n_units),
+			l3=L.Linear(n_units, n_out),
+		)
+
+	def __call__(self, x, y):
+		t = self.predict(x)
+		self.loss =  F.mean_squared_error(y, t)
+		return self.loss
+
+	def predict(self, x):
+		h1 = F.relu(self.l1(x))
+		h2 = F.relu(self.l2(h1))
+		t = self.l3(h2)
+		return t
+
+
+class shoot_network():
+
+	def __init__(self,units=300):
+		self.units = units
+		self.model = None
+
+	def fit(self,x_train,y_train,n_epoch=100, batchsize=30):
+		x_train = np.array(x_train, np.float32)
+		y_train = np.array(y_train, np.float32).reshape(len(y_train),1)
+		self.train(x_train, y_train, n_epoch=n_epoch, batchsize=30)
+
+	def train(self, x_train, y_train,n_epoch=100, batchsize=30):
+		self.model = network(len(x_train[0]),self.units,1)
+		xp = np
+		optimizer = optimizers.Adam()
+		optimizer.setup(self.model)
+		N = len(x_train)
+		for epoch in six.moves.range(1, n_epoch + 1):
+			print('epoch', epoch)
+			# training
+			perm = np.random.permutation(N)
+			sum_accuracy = 0
+			sum_loss = 0
+			for i in six.moves.range(0, N, batchsize):
+				x = chainer.Variable(xp.asarray(x_train[perm[i:i + batchsize]]))
+				t = chainer.Variable(xp.asarray(y_train[perm[i:i + batchsize]]))
+				# Pass the loss function (Classifier defines it) and its arguments
+				optimizer.update(self.model, x, t)
+
+				sum_loss += float(self.model.loss.data) / N
+
+			print 'epoch %d mean_squared_error:%f' % (epoch,sum_loss)
+
+
+	def predict(self,test_x):
+		x = chainer.Variable(np.asarray(test_x, np.float32))
+		t = self.model.predict(x)
+		return t.data
+
+
+
+
+
+
+
---
+++ src/solver.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import cross_validation
+import model
+
+
+
+def factorize(data):
+	return (data - np.mean(data,axis = 0)) / np.var(data,axis = 0)
+
+
+N = 30696
+M = 5000
+
+data = pd.read_csv('../data/data.csv')
+output = open('output.csv', 'w')
+
+output.write('shot_id,shot_made_flag\n')
+
+
+train_lst = ['shot_id','lat','loc_x','loc_y','lon','minutes_remaining','playoffs','seconds_remaining','shot_distance','shot_made_flag']
+lst = ['lat','loc_x','loc_y','lon','minutes_remaining','playoffs','seconds_remaining','shot_distance','shot_made_flag']
+cols = ['combined_shot_type', 'shot_zone_range', 'season','period']
+# lst = ['playoffs','seconds_remaining','shot_distance']
+
+
+data_x = data[lst]
+
+
+#label to binary features
+# data_sparse = data['shot_id']
+
+
+for col in cols:
+	d = pd.get_dummies(data[col])
+	data_x = pd.concat((data_x,d),axis=1)
+
+
+# data_sparse.drop('shot_id', axis=1)
+# U,s,V = np.linalg.svd(data_sparse.values,full_matrices=False)
+# d = 8
+# svd_features = np.dot(U[:,0:d],np.dot(np.diag(s[:d]),V.T[:,0:d].T))
+# print svd_features
+#
+
+
+data_x = factorize(data_x)
+train_x = data_x[-pd.isnull(data_x.shot_made_flag)]
+test_x = data_x[pd.isnull(data_x.shot_made_flag)]
+del test_x['shot_made_flag']
+del train_x['shot_made_flag']
+test_x = test_x.values
+train_x = train_x.values
+train_y = data[-pd.isnull(data_x.shot_made_flag)]['shot_made_flag'].values
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_x, train_y, test_size=0.4, random_state=0)
+sn = model.shoot_network(units=150)
+
+sn.fit(train_x,train_y,n_epoch=200,batchsize=1000)
+# result = np.sqrt(np.sum(np.square(y-y_test)))
+#
+# units = [100,120,150,200,250,300]
+# f = open('cross.txt','w')
+# for u in units:
+# 	X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_x, train_y, test_size=0.4, random_state=0)
+# 	sn = model.shoot_network()
+# 	sn.fit(train_x,train_y,n_units=u,n_epoch=150,batchsize=100)
+# 	y = sn.predict(X_test)
+# 	# y[y > 0.60] = 1
+# 	# y[y < 0.40] = 0
+# 	result = np.sqrt(np.sum(np.square(y-y_test)))
+# 	f.write(str(u)+ ' ' + str(result) + " " + str(np.mean(y)) + '\n')
+
+
+ans = sn.predict(test_x)
+count  = 0
+test_data = data[pd.isnull(data.shot_made_flag)]
+for i,row in test_data.iterrows():
+	#print count,i
+	result = ans[count][0]
+	if result > 1: result = 1
+	elif result < 0: result = 0
+	output.write(str(row['shot_id'])+","+str(result)+'\n')
+	count += 1
+
+output.close()
+#
+#
